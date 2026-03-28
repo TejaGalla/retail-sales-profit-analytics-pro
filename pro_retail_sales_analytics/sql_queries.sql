c6d7198537e97a13630ca13523f6bc294054ebe4
GALLA DIVYA TEJA <165240675+TejaGalla@users.noreply.github.com>
2026-03-28 16:27:31 +0530

    Update sql_queries.sql

--- pro_retail_sales_analytics/sql_queries.sql
+++ pro_retail_sales_analytics/sql_queries.sql
@@ -1,15 +1,14 @@
--- Use table name retail_sales after importing CSV
-
 SELECT ROUND(SUM(gross_revenue),2) AS total_revenue,
        ROUND(SUM(profit),2) AS total_profit,
        ROUND((SUM(profit)/NULLIF(SUM(gross_revenue),0))*100,2) AS profit_margin_pct
 FROM retail_sales;
-
+-------------------------------------------------------------------------------------
 SELECT DATE_FORMAT(order_date, '%Y-%m') AS month,
        ROUND(SUM(gross_revenue),2) AS revenue
 FROM retail_sales
 GROUP BY DATE_FORMAT(order_date, '%Y-%m')
 ORDER BY month;
+---------------------------------------------------------------------------------------
 
 SELECT category,
        ROUND(SUM(gross_revenue),2) AS revenue,
@@ -17,6 +16,7 @@ SELECT category,
 FROM retail_sales
 GROUP BY category
 ORDER BY revenue DESC;
+---------------------------------------------------------------------------------------
 
 SELECT region,
        ROUND(SUM(gross_revenue),2) AS revenue,
@@ -25,19 +25,21 @@ SELECT region,
 FROM retail_sales
 GROUP BY region
 ORDER BY revenue DESC;
+----------------------------------------------------------------------------------------
 
 SELECT product_name, ROUND(SUM(gross_revenue),2) AS revenue
 FROM retail_sales
 GROUP BY product_name
 ORDER BY revenue DESC
 LIMIT 10;
-
+----------------------------------------------------------------------------------------
 SELECT discount_pct,
        ROUND(SUM(gross_revenue),2) AS revenue,
        ROUND(SUM(profit),2) AS profit
 FROM retail_sales
 GROUP BY discount_pct
 ORDER BY discount_pct;
+----------------------------------------------------------------------------------------
 
 WITH regional_category AS (
     SELECT region, category, ROUND(SUM(gross_revenue),2) AS revenue,
